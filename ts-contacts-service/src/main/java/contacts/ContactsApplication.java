e3edd10af1ffcf62bcd9d7f2985fdca1aee0bc11
Jiahong <JoeZJiahong@gmail.com>
2019-10-21 16:55:20 +0800

    MOD: add import

--- ts-contacts-service/src/main/java/contacts/ContactsApplication.java
+++ ts-contacts-service/src/main/java/contacts/ContactsApplication.java
@@ -1,5 +1,6 @@
 package contacts;
 
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
